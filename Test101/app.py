3525b0d0ced545655c2fe557c6782c949b781e38
lhshuvo <36121860+lhshuvo@users.noreply.github.com>
2023-01-23 12:24:52 +0600

    Add files via upload

--- Test101/app.py
+++ Test101/app.py
@@ -4,24 +4,34 @@ import requests
 from werkzeug.utils import secure_filename
 import os
 
+# Initialize a Flask application
 app = Flask("Test101")
+
+# Set the location of the folder where uploaded files will be saved
 app.config['UPLOAD_FOLDER'] = './'
 
+# Function to check if the file being uploaded is in the allowed file types
 def allowed_file(filename):
+    # Return true if the file extension is in the allowed file types
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ['xlsx', 'xls']
 
+# Route for the form submission
 @app.route('/', methods = ['GET', 'POST'])
 def myform():
     if request.method == 'POST':
+        # Check if the form field 'x' (file input) is not present in the submitted form
         if 'x' not in request.files:
             flash('No file part')
             return redirect(request.url)
         file = request.files['x']
+        # Check if no file was selected for upload
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
+        # Check if the file being uploaded is allowed
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
+            # Save the uploaded file to the specified folder
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             df = pd.read_excel(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             responsess = []
@@ -50,4 +60,5 @@ def myform():
             return render_template('output.html')
     return render_template('index.html')
 
+# Start the application
 app.run(port=1234, debug=True)
